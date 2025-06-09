1014765ee6aa1f2ff3515f2823a6bf5e9955dc3d
FriendlyJJC <bruck708@gmail.com>
2025-06-09 14:53:32 +0200

    API Finished API Routes with Subrouting Logic for API Versioning

--- apiv1/apiv1.go
+++ apiv1/apiv1.go
@@ -2,15 +2,23 @@ package apiv1
 
 import (
 	"encoding/json"
+	"fmt"
+	"math/rand"
 	"net/http"
 )
 
-func GenerateID() (string, error) {
-
+func GenerateID() string {
+	const length int8 = 7
+	id := make([]byte, length)
+	characters := "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890_?!*+&"
+	for i := 0; i < int(length); i++ {
+		id[i] = characters[rand.Intn(len(characters))]
+	}
+	return string(id)
 }
 
 type AddURLBody struct {
-	LongURl  string  `json:"longurl"`
+	LongURL  string  `json:"longurl"`
 	ShortURL *string `json:"shorturl"`
 }
 
@@ -18,11 +26,23 @@ func AddURL(w http.ResponseWriter, r *http.Request) {
 	var ReqBody AddURLBody
 	err := json.NewDecoder(r.Body).Decode(&ReqBody)
 	if err != nil {
-		http.Error(w, "JSON could not be Encoded, try again", http.StatusInternalServerError)
+		http.Error(w, "JSON could not be decoded, try again", http.StatusInternalServerError)
+		return
 	}
-
+	generated_id := GenerateID()
+	ReqBody.ShortURL = &generated_id
+	fmt.Println(ReqBody)
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("ShortURL was successfully created"))
 }
 
-func APIHandleV1() {
+func Test(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("It works"))
+}
+
+func APIHandleV1(w http.ResponseWriter, r *http.Request) {
 	v1_mux := http.NewServeMux()
+	v1_mux.HandleFunc("/shorturl/add", AddURL)
+	v1_mux.HandleFunc("/test", Test)
+	v1_mux.ServeHTTP(w, r)
 }
--- main.go
+++ main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"net/http"
+
+	"github.com/FriendlyJJC/api_server/apiv1"
 )
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/v1/", http.StripPrefix("/v1", http.HandlerFunc(apiv1.APIHandleV1)))
 
+	http.ListenAndServe(":8080", mux)
 }
