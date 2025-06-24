d1233f743359f6fc7d8dc32fd4470388b87c46fc
FriendlyJJC <bruck708@gmail.com>
2025-06-24 22:10:56 +0200

    Added Test for Get Route and fixed tests

--- tests/apiv1_test.go
+++ tests/apiv1_test.go
@@ -9,14 +9,28 @@ import (
 	"github.com/FriendlyJJC/api_server/apiv1"
 )
 
+const TEST_URL = "http://localhost:8080/v1"
+
 func TestAddUrl(t *testing.T) {
+	//Create a Test Route that makes an Http Req to the Post Route with Value and checks if StatusCode is 200
 	body := apiv1.AddURLBody{LongURL: "youtube.com"}
 	json_body, _ := json.Marshal(body)
-	req, req_err := http.Post("http://localhost:8080/v1/shorturl/add", "application/json", strings.NewReader(string(json_body)))
+	req, req_err := http.Post(TEST_URL+"/shorturl/add", "application/json", strings.NewReader(string(json_body)))
 	if req_err != nil {
-		t.Fatalf("Something is happend with the Request: %v", req_err)
+		t.Fatalf("Something went wrong with the Request: %v", req_err)
 	}
 	if req.StatusCode != 200 {
-		t.Errorf("The Reponse Code is not 200 except it is %d", req.StatusCode)
+		t.Errorf("The Reponse Code is not 200 as expected. actual: %d and url was %s", req.StatusCode, req.Request.URL)
+	}
+}
+
+func TestGetUrl(t *testing.T) {
+	//Create a Test Route that makes an Http Req the GET Route and check if StatusCode is 200
+	req, req_err := http.Get(TEST_URL + "/shorturl/get")
+	if req_err != nil {
+		t.Fatalf("Something went wrong with the Request: %v", req_err)
+	}
+	if req.StatusCode != 200 {
+		t.Errorf("The Response Code is not 200 as expected. actual: %d", req.StatusCode)
 	}
 }
