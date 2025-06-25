56cf5e9d81620a252fc4f0609fa33d693261de40
FriendlyJJC <bruck708@gmail.com>
2025-06-25 20:55:25 +0200

    Added new TestCase for GET/:ID Route

--- tests/apiv1_test.go
+++ tests/apiv1_test.go
@@ -12,7 +12,7 @@ import (
 const TEST_URL = "http://localhost:8080/v1"
 
 func TestAddUrl(t *testing.T) {
-	//Create a Test Route that makes an Http Req to the Post Route with Value and checks if StatusCode is 200
+	//Create a Test that makes an Http Req to the Post Route with Value and checks if StatusCode is 200
 	body := apiv1.AddURLBody{LongURL: "youtube.com"}
 	json_body, _ := json.Marshal(body)
 	req, req_err := http.Post(TEST_URL+"/shorturl/add", "application/json", strings.NewReader(string(json_body)))
@@ -24,13 +24,32 @@ func TestAddUrl(t *testing.T) {
 	}
 }
 
-func TestGetUrl(t *testing.T) {
-	//Create a Test Route that makes an Http Req the GET Route and check if StatusCode is 200
+func TestGetUrls(t *testing.T) {
+	//Create a Test that makes an Http Req the GET Route and check if StatusCode is 200
 	req, req_err := http.Get(TEST_URL + "/shorturl/get")
 	if req_err != nil {
 		t.Fatalf("Something went wrong with the Request: %v", req_err)
 	}
 	if req.StatusCode != 200 {
 		t.Errorf("The Response Code is not 200 as expected. actual: %d", req.StatusCode)
+	}
+}
+
+func TestGetUrl(t *testing.T) {
+	var TEST_SHORTURL string
+	//Create a Test that makes an HTTP Req to the GET/:ID Route to Get a Specific URL from the shorturl
+	req, req_error := http.Get(TEST_URL + "/shorturl/get")
+	if req_error != nil {
+		t.Fatalf("Something went wrong with the GET Req to get all IDS: %v", req_error)
+	}
+	var ResponseBody apiv1.ShortURLS
+	json.NewDecoder(req.Body).Decode(&ResponseBody)
+	TEST_SHORTURL = *ResponseBody.Data[0].ShortURL
+	req_2, req_2_error := http.Get(TEST_URL + "/shorturl/get/" + TEST_SHORTURL)
+	if req_2_error != nil {
+		t.Fatalf("Something went wrong with the GET Req with the shorturl")
+	}
+	if req_2.StatusCode != 200 {
+		t.Errorf("The Response Code is not 200 as expected. actual: %d", req_2.StatusCode)
 	}
 }
