002e6387881c0da5d3b375f347f1599be3d1084d
FriendlyJJC <bruck708@gmail.com>
2025-06-26 22:12:28 +0200

    Added GORM and wrote intializing functions

--- db/db.go
+++ db/db.go
@@ -1,1 +1,28 @@
 package db
+
+import (
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
+)
+
+type ShortUrls struct {
+	gorm.Model
+	longurl  string
+	shorturl string
+}
+
+func InitializeDB() *gorm.DB {
+	db, err := gorm.Open(sqlite.Open("urls.db"), &gorm.Config{})
+	if err != nil {
+		panic("failed to connect to database")
+	}
+	return db
+}
+
+func Migrate(db *gorm.DB) (ok bool) {
+	err := db.AutoMigrate(&ShortUrls{})
+	if err != nil {
+		return false
+	}
+	return true
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,13 @@
 module github.com/FriendlyJJC/api_server
 
 go 1.24.2
+
+require gorm.io/driver/sqlite v1.6.0
+
+require (
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	github.com/mattn/go-sqlite3 v1.14.28 // indirect
+	golang.org/x/text v0.26.0 // indirect
+	gorm.io/gorm v1.30.0 // indirect
+)
